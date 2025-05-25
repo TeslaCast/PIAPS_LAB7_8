4368adef53315f1e08c2c525fb805136524e7e9b
Antone4 <vovn.pro@mail.ru>
2025-05-25 17:09:45 +0300

    Core

---
+++ main.py
@@ -0,0 +1,89 @@
+import pygame
+from typing import Protocol, Callable, runtime_checkable
+
+class Drawable(Protocol):
+    def draw(self, screen) -> None: ...
+
+@runtime_checkable
+class Clickable(Protocol):
+    def click(self, screen) -> Callable[[], None]: ...
+    def collidepoint(self, point) -> bool: ...
+
+class Button:
+    def __init__(self, x: int, y: int, width: int, height: int, color):
+        self.button_surface = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.button_surface.fill(color)
+    def draw(self, screen):
+        screen.blit(self.button_surface, (self.x, self.y))
+        #pygame.draw.rect(surface=screen, color=self.color, rect=self.button_rect)
+    def click(self, screen) -> Callable[[], None]:
+        def func():
+            pass
+        return func
+    def collidepoint(self, point) -> bool:
+        (x, y) = point
+        # print(point)
+        # print((self.x, self.y))ы
+        return (y >= self.y and y <= self.y + self.height) and (x >= self.x and x <= self.x + self.width)
+
+class Game:
+    def __init__(self, gameName: str):
+        pygame.init()
+        self.__screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption(gameName)
+        self.__running = False
+        self.__clock = pygame.time.Clock()
+        self.__objects: list[Drawable] = []
+        self.__clickable_objects: list[Clickable] = [] 
+    def Run(self):
+        self.__running = True
+        #button_rect = pygame.Rect(100, 100, 200, 50)
+        while self.__running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.__running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.__running = False
+                    elif event.key == pygame.K_SPACE:
+                        print("Пробел нажат!")
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for clicked in self.__clickable_objects:
+                        # sup = pygame.Rect(0, 0, 0, 0) 
+                        # sup.collidepoint
+                        if clicked.collidepoint(event.pos):
+                            clicked.click(self.__screen)
+                            print("Кнопка нажата!")  
+                    # if button_rect.collidepoint(event.pos):
+                    #     print("Кнопка нажата!")
+
+            self.__screen.fill((176, 224, 230))  # фон
+            self.render_all(self.__screen)
+            # pygame.draw.circle(self.__screen, (255, 0, 0), (320, 240), 50)  # Красный круг
+            # pygame.draw.rect(self.__screen, (0, 255, 0), button_rect)
+
+            pygame.display.flip()
+            self.__clock.tick(60)  # Ограничить до 60 кадров в секунду
+
+        pygame.quit()
+    def render_all(self, screen):
+        for obj in self.__objects:
+            obj.draw(screen)
+    def add_obj(self, obj: Drawable):
+        self.__objects.append(obj)
+        if isinstance(obj, Clickable):
+            self.__clickable_objects.append(obj)
+        
+if __name__ == "__main__":
+    game = Game("Простая игра")
+    button = Button(100,100, 100, 100, (255, 0, 0))
+    game.add_obj(button)
+    button = Button(100,200, 100, 100, (0, 0, 0))
+    game.add_obj(button)
+    game.Run()
+    
