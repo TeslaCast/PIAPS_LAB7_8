406df540c67db70f3344b236172d54378a5f21b1
Antone4 <vovn.pro@mail.ru>
2025-05-27 20:37:48 +0300

    Add text to button

--- main.py
+++ main.py
@@ -13,8 +13,17 @@ class Button:
         self.height = height
         self.color = color
         self.func = func
+        ##########
+        self.font = pygame.font.SysFont("arial", 24)
+        self.text_surface = self.font.render("Нажми", True, (255, 255, 255))  # Белый текст
+        self.text_rect = self.text_surface.get_rect(center=(self.x+self.width//2, self.y+self.height//2))  # Центр кнопки
+        self.button_surface.blit(self.text_surface, self.text_rect)
+
     def draw(self, screen):
         self.button_surface.fill(self.color)
+        # пересчитываем позицию текста (на случай перемещений, не строго обязательно)
+        self.text_rect = self.text_surface.get_rect(center=(self.width // 2, self.height // 2))
+        self.button_surface.blit(self.text_surface, self.text_rect)
         screen.blit(self.button_surface, (self.x, self.y))
         #pygame.draw.rect(surface=screen, color=self.color, rect=self.button_rect)
     def click(self, screen) -> None:
